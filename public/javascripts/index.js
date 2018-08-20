9458e94e3a6a373201cad44d6d9b15e7deca3430
luyiming <luyimingchn@gmail.com>
2018-08-20 22:41:06 +0800

    add image modal

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,6 @@
+var g_page = 1;
+var g_per_page = 30;
+
 $(document).ready(function () {
     $.ajax('api/get_top_places', {
             data: {
@@ -8,28 +11,70 @@ $(document).ready(function () {
             // console.log(places_list);
             $('#top-places-spinner').hide();
             $('#top-places-list').append(places_list);
+
+            $('.searchable-item').click(function () {
+                do_text_search($(this).text(), 1, 20);
+            });
         })
         .fail(err => {
             console.log('get_top_places error:');
             console.log(err);
         });
+
+    do_text_search('mountain', 1, 20);
+
 });
 
-$('#search-text-button').click(function () {
-    $.ajax('api/search_by_text', {
-        data: {
-            text: $('#search-text-input').val(),
-            page: 1,
-            per_page: 20
-        }
-    })
-    .done(photo_list => {
-        // console.log(photo_list);
-        $('#image-gallery').html(photo_list);
-    })
-    .fail(err => {
-        console.log('get_top_places error:');
-        console.log(err);
-    });
+function get_image_modal(photo_id) {
+    $.ajax('api/get_image_modal', {
+            data: {
+                photo_id: photo_id
+            }
+        })
+        .done(photo_modal => {
+            $('#modal-container').html(photo_modal);
+            $('#sp-image-modal').modal('show');
 
+            $('.searchable-item').click(function () {
+                do_text_search($(this).text(), 1, 20);
+                $('#sp-image-modal').modal('hide');
+            });
+        })
+        .fail(err => {
+            console.log('get_top_places error:');
+            console.log(err);
+        });
+}
+
+function do_text_search(text, page, per_page) {
+    g_page = page;
+    g_per_page = per_page;
+    $.ajax('api/search_by_text', {
+            data: {
+                text: text,
+                page: page,
+                per_page: per_page
+            }
+        })
+        .done(photo_list => {
+            $('#image-gallery').html(photo_list);
+
+            $('.image-show-button').click(function () {
+                let photo_id = $(this).parent().parent().prev().data('id');
+                get_image_modal(photo_id);
+            });
+        })
+        .fail(err => {
+            console.log('get_top_places error:');
+            console.log(err);
+        });
+}
+
+$('#search-text-button').click(function () {
+    do_text_search($('#search-text-input').val(), 1, 20);
+});
+
+$('.image-show-button').click(function () {
+    let photo_id = $(this).parent().parent().prev().data('id');
+    get_image_modal(photo_id);
 });
--- routes/api.js
+++ routes/api.js
@@ -32,4 +32,43 @@ router.get('/search_by_text', function (req, res) {
         });
 });
 
+router.get('/get_image_modal', function (req, res) {
+    flickr.get_photo_info(req.query.photo_id)
+        .then(info => {
+            // console.log(JSON.stringify(info));
+            modal_photo = {};
+            modal_photo['src'] = flickr.get_photo_url(info['photo']);
+            modal_photo['owner'] = info['photo']['owner']['realname'] || info['photo']['owner']['username'];
+            modal_photo['title'] = info['photo']['title']['_content'] || 'Photo';
+
+            modal_photo['tags'] = [];
+
+            for (let element of info['photo']['tags']['tag']) {
+                modal_photo['tags'].push(element['_content']);
+            }
+
+            modal_photo['places'] = [];
+            if (info['photo']['location']) {
+                if (info['photo']['location']['locality']) {
+                    modal_photo['places'].push(info['photo']['location']['locality']['_content']);
+                }
+                if (info['photo']['location']['region']) {
+                    modal_photo['places'].push(info['photo']['location']['region']['_content']);
+                }
+                if (info['photo']['location']['country']) {
+                    modal_photo['places'].push(info['photo']['location']['country']['_content']);
+                }
+            }
+
+            // console.log(JSON.stringify(modal_photo, null, 2));
+
+            res.render('components/image-modal', {
+                modal_photo: modal_photo
+            });
+        })
+        .catch(err => {
+            res.sendStatus(404);
+        });
+});
+
 module.exports = router;
